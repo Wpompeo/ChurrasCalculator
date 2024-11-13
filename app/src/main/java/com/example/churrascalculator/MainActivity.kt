0e184e82a7fd49ffbc22d353095023efa53de4ba
Wpompeo <wag_pompeo@yahoo.com.br>
2024-11-13 10:15:09 -0300

    second commit

--- app/src/main/java/com/example/churrascalculator/MainActivity.kt
+++ app/src/main/java/com/example/churrascalculator/MainActivity.kt
@@ -1,16 +1,51 @@
 package com.example.churrascalculator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val edtAdults = findViewById<TextInputEditText>(R.id.edt_adultos)
+        val edtChildren = findViewById<TextInputEditText>(R.id.edt_criancas)
+        val edtHours = findViewById<TextInputEditText>(R.id.edt_horas)
+        val btnCalculate = findViewById<Button>(R.id.btn_calc)
+
+        btnCalculate.setOnClickListener {
+            val adultsStr: String = edtAdults.text.toString()
+            val childrenStr: String = edtChildren.text.toString()
+            val hoursStr: String = edtHours.text.toString()
+
+            if( adultsStr == "" || childrenStr == "" || hoursStr == ""  ){
+                Snackbar.make(
+                    edtAdults,
+                    "Preencha todos os campos!",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            } else {
+
+                val adults = adultsStr.toFloat()
+                val children = childrenStr.toFloat()
+                val hours = hoursStr.toFloat()
+
+                val childrenMeat = children * 0.100
+                val adultsMeat = adults * 0.300
+                val beerAdults = adults * 4 * hours
+                val refrigeratorChildren = (adults + children) * 0.400
+
+                val intent = Intent(this, ResultCalculator::class.java)
+                startActivity(intent)
+            }
+
+
+        }
     }
 }
