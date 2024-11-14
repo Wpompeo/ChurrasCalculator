7e51d746e9e5f1bc434146f6b80068390fa7dcf1
Wpompeo <wag_pompeo@yahoo.com.br>
2024-11-14 00:55:30 -0300

    finally commit

--- app/src/main/java/com/example/churrascalculator/MainActivity.kt
+++ app/src/main/java/com/example/churrascalculator/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val edtAdults = findViewById<TextInputEditText>(R.id.edt_adultos)
+        edtAdults.text?.clear()
         val edtChildren = findViewById<TextInputEditText>(R.id.edt_criancas)
         val edtHours = findViewById<TextInputEditText>(R.id.edt_horas)
         val btnCalculate = findViewById<Button>(R.id.btn_calc)
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             val childrenStr: String = edtChildren.text.toString()
             val hoursStr: String = edtHours.text.toString()
 
-            if( adultsStr == "" || childrenStr == "" || hoursStr == ""  ){
+            if (adultsStr == "" || childrenStr == "" || hoursStr == "") {
                 Snackbar.make(
                     edtAdults,
                     "Preencha todos os campos!",
@@ -36,16 +37,35 @@ class MainActivity : AppCompatActivity() {
                 val children = childrenStr.toFloat()
                 val hours = hoursStr.toFloat()
 
-                val childrenMeat = children * 0.100
-                val adultsMeat = adults * 0.300
-                val beerAdults = adults * 4 * hours
-                val refrigeratorChildren = (adults + children) * 0.400
 
-                val intent = Intent(this, ResultCalculator::class.java)
-                startActivity(intent)
+
+                if (hours.toInt() == 1 || hours.toInt() == 2) {
+                    val childrenMeat = children * 200 / 1000
+                    val adultsMeat = adults * 400 / 1000
+                    val meat = childrenMeat + adultsMeat
+                    val beerAdults = adults * 4 * hours
+                    val refrigeratorChildren = (adults + children) * 400 / 1000
+
+                    val intent = Intent(this, ResultCalculator::class.java)
+                    intent.putExtra(KEY_RESULT_BEER, beerAdults)
+                    intent.putExtra(KEY_RESULT_MEAT, meat)
+                    intent.putExtra(KEY_RESULT_DRINKS, refrigeratorChildren)
+                    startActivity(intent)
+
+                } else {
+                    val childrenMeat = children * 250 / 1000
+                    val adultsMeat = adults * 500 / 1000
+                    val meat = childrenMeat + adultsMeat
+                    val beerAdults = adults * ( 45 / 10 ) * hours
+                    val refrigeratorChildren = (adults + children) * 600 / 1000
+
+                    val intent = Intent(this, ResultCalculator::class.java)
+                    intent.putExtra(KEY_RESULT_BEER, beerAdults)
+                    intent.putExtra(KEY_RESULT_MEAT, meat)
+                    intent.putExtra(KEY_RESULT_DRINKS, refrigeratorChildren)
+                    startActivity(intent)
+                }
             }
-
-
         }
     }
 }
--- app/src/main/java/com/example/churrascalculator/ResultCalculator.kt
+++ app/src/main/java/com/example/churrascalculator/ResultCalculator.kt
@@ -1,15 +1,40 @@
 package com.example.churrascalculator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+const val KEY_RESULT_BEER = "ResultCalculator.KEY_BEER"
+const val KEY_RESULT_MEAT = "ResultCalculator.kEY_MEAT"
+const val KEY_RESULT_DRINKS = "ResultCalculator.KEY_DRINKS"
 
 class ResultCalculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_calculator)
 
+        val resultBeer = intent.getFloatExtra(KEY_RESULT_BEER, 0f)
+        val resultMeat = intent.getFloatExtra(KEY_RESULT_MEAT, 0f)
+        val resultDrinks = intent.getFloatExtra(KEY_RESULT_DRINKS, 0f)
+
+        val meat = findViewById<TextView>(R.id.tv_carne)
+        val meatFormat = String.format("%.2f", resultMeat)
+        "${meatFormat}(kg)".also { meat.text = it }
+
+        val beer = findViewById<TextView>(R.id.tv_cerveja)
+        val beerFormat = String.format("%.2f", resultBeer)
+        "${beerFormat}(Latas)".also { beer.text = it }
+
+        val drink = findViewById<TextView>(R.id.tv_refrigerante)
+        val drinkFormat = String.format("%.2f", resultDrinks)
+        "${drinkFormat}(Litros)".also { drink.text = it }
+
+        val btnRecall = findViewById<Button>(R.id.btn_recalc)
+        btnRecall.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
